caa8e15e3e96173b0550a53b50586c1e4e541b33
GianatiempoJulian <gianatiempojulian@gmail.com>
2024-03-19 20:08:32 -0300

    0.3 | Lottery check styles added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@
 export function Button ({ children }){
     return(
         <button className="bg-amber-600 w-3/6 h-16 rounded-md text-blue-800
-        font-bold hover:bg-amber-700 hover:text-amber-800 focus:ring focus:ring-white-300 transition delay-100">
+        font-bold hover:bg-amber-700 hover:text-amber-800 focus:ring focus:ring-white-300 transition delay-100" >
            {children}</button>
     )
 }
--- src/components/CheckResultCard.jsx
+++ src/components/CheckResultCard.jsx
@@ -1,24 +1,29 @@
+import { Button } from './Button';
+
 export function CheckResultCard ({}){
     return(
-        <article className='checkResult-card'>
-            <header className='checkResult-card-header'>
-               <h1>Control de CARTÓN</h1>
+        <article className='checkResult-card bg-emerald-700 '>
+            <header className='checkResult-card-header grid w-full h-full justify-center justify-items-center'>
+            <strong className="bg-emerald-400 text-blue-800 rounded-md h-12 w-56 mt-5 flex items-center justify-center">Control de cartón</strong>
                <div className="checkResult-data gap-6 p-6">
                     <form action="">
-                        <div className="p-2">
-                            <label className="w-12 mr-5" htmlFor="lottery">N° de sorteo</label>
-                            <input type="text" name="lottery" placeholder="2330"/>
+                        <div className="p-2 grid grid-cols-2 w-full">
+                            <label className="text-white font-bold ml-4" htmlFor="lottery">N° sorteo</label>
+                            <input type="text" name="lottery" className="focus:ring focus:ring-green-300  h-8" placeholder="2330"/>
                         </div>
-                        <div className="p-2">
-                            <label className="w-12 mr-5" htmlFor="number">N° de cartón</label>
-                            <input type="text" name="number" placeholder="número"/>
+                        <div className="p-2 grid grid-cols-2">
+                            <label className="text-white font-bold ml-4 text-sm" htmlFor="number">N° cartón</label>
+                            <input type="text" name="number" className="focus:ring focus:ring-green-300 h-8" placeholder="XX.XX.XXX"/>
                         </div>
-                        <div className="p-2">
-                            <label className="w-12 mr-5" htmlFor="algorithm ">Algoritmo</label>
-                            <input type="text" name="algorithm " placeholder="algoritmo"/>
+                        <div className="p-2 grid grid-cols-2">
+                            <label className="text-white font-bold ml-4 text-sm" htmlFor="algorithm ">Algoritmo</label>
+                            <input type="text" name="algorithm " className="focus:ring focus:ring-green-300  h-8" placeholder="Ultimos 2 digitos"/>
                         </div>
                     </form>
-               </div>
+                    </div>
+                    <Button>
+                            Controlar
+                    </Button>
             </header>
         </article>
     )
